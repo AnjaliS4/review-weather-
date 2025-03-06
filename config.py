ce1cd511fad8599cc4a165042e9027b4ff8047f8
Anjali Simkhada <anjalisimkhada5@gmail.com>
2025-03-06 16:05:54 +0545

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:ANJALI999@localhost:5432/aggregator')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     OPENWEATHER_API_KEY = os.environ.get('OPENWEATHER_API_KEY')
     NEWSAPI_API_KEY = os.environ.get('NEWSAPI_API_KEY')
